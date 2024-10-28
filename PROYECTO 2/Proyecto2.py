1f39e75e44b0add361aab8e62fbe16dbc9cf1bd9
sa.gomezg1 <sa.gomezg1@uniandes.edu.co>
2024-10-28 09:52:18 -0500

    Limpieza de Datos

--- PROYECTO 2/Proyecto2.py
+++ PROYECTO 2/Proyecto2.py
@@ -2,12 +2,15 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
+
+#LIMPIEZA DE DATOS
+
 # Load the dataset
 file_path = file_path = 'C:/Users/saral/OneDrive - Universidad de los andes/ARCHVIOS MAC SARA/SARA GG 8/OCTAVO SEMESTRE/ANALITICA/PROYECTO 2/bank-full.csv'
 
 data = pd.read_csv(file_path, sep=';')
 
-# Step 1: Handle categorical data by encoding
+
 # Label encoding for binary categorical columns
 binary_columns = ['default', 'housing', 'loan', 'y']
 for col in binary_columns:
@@ -21,7 +24,7 @@ data['education'].fillna(data['education'].mode()[0], inplace=True)
 multi_category_columns = ['job', 'marital', 'education', 'contact', 'month', 'poutcome']
 data = pd.get_dummies(data, columns=multi_category_columns, drop_first=True)
 
-# Step 2: Handle 'unknown' values in categorical columns
+
 # Replace 'unknown' with a more appropriate label or strategy (e.g., 'unknown' -> NaN, then impute or remove)
 data.replace('unknown', pd.NA, inplace=True)
 
@@ -31,3 +34,4 @@ data.fillna(data.mode().iloc[0], inplace=True)
 # Step 3: Verify the data is ready for analysis
 print(data.info())
 print(data.head())
+
