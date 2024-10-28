bc1e690f433dc1ad72c742cc6e7f2984c51d2643
sa.gomezg1 <sa.gomezg1@uniandes.edu.co>
2024-10-28 10:48:39 -0500

    Exploración de datos

--- PROYECTO 2/Proyecto2.py
+++ PROYECTO 2/Proyecto2.py
@@ -2,6 +2,8 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #LIMPIEZA DE DATOS
 
@@ -34,4 +36,61 @@ data.fillna(data.mode().iloc[0], inplace=True)
 # Step 3: Verify the data is ready for analysis
 print(data.info())
 print(data.head())
+print(data.columns)
 
+
+#EXPLORACION DE DATOS
+
+# Histogramas
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+data['age'].hist(ax=axes[0, 0])
+axes[0, 0].set_title('Histograma de Edad')
+
+data['balance'].hist(ax=axes[0, 1])
+axes[0, 1].set_title('Histograma de Saldo')
+    
+sns.countplot(x='housing', data=data, ax=axes[1, 0])
+axes[1, 0].set_title('Distribución de Crédito de Vivienda')
+
+sns.countplot(x='loan', data=data, ax=axes[1, 1])
+axes[1, 1].set_title('Distribución de Crédito Personal')
+plt.tight_layout()
+plt.show()
+
+#Diagrama de Cajas
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+data.boxplot(column='age', ax=axes[0])
+axes[0].set_title('Diagrama de Caja de Edad')
+data.boxplot(column='balance', ax=axes[1])
+axes[1].set_title('Diagrama de Caja de Saldo')
+plt.tight_layout()
+plt.show()
+
+# Diagramas de violín
+
+# Crear una nueva columna que indique el tipo de contacto
+data['contact'] = data[['contact_telephone', 'contact_unknown']].idxmax(axis=1)
+
+# Reemplazar los nombres de las columnas para que sean más descriptivos
+data['contact'] = data['contact'].replace({'contact_telephone': 'Telephone', 'contact_unknown': 'Unknown'})
+
+# Graficar el diagrama de violín con la nueva columna 'contact'
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.violinplot(x='contact', y='duration', data=data, ax=ax)
+ax.set_title('Distribución de Duración de Campaña por Tipo de Contacto')
+ax.set_xlabel('Tipo de Contacto')
+ax.set_ylabel('Duración de Campaña (días)')
+plt.tight_layout()
+plt.show()
+
+
+# Diagramas de dispersión
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+data.plot(kind='scatter', x='age', y='balance', ax=axes[0])
+axes[0].set_title('Edad vs. Saldo')
+
+data.plot(kind='scatter', x='age', y='y', ax=axes[1])
+axes[1].set_title('Edad vs. y')
+plt.tight_layout()
+plt.show()
